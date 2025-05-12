77cd9c3476b259abf2abb55d473fcbfb93f8b88e
Maxim Gribencicov <max7gri@gmail.com>
2025-05-12 21:02:47 +0300

    Infra changes

---
+++ tag-and-push-images.sh
@@ -0,0 +1,11 @@
+gcloud auth login
+
+docker tag com.food.ordering.system/order.service:$1 europe-central2-docker.pkg.dev/sunlit-vortex-459613-h0/food-ordering-system-repository/order.service:$1
+docker tag com.food.ordering.system/customer.service:$1 europe-central2-docker.pkg.dev/sunlit-vortex-459613-h0/food-ordering-system-repository/customer.service:$1
+docker tag com.food.ordering.system/payment.service:$1 europe-central2-docker.pkg.dev/sunlit-vortex-459613-h0/food-ordering-system-repository/payment.service:$1
+docker tag com.food.ordering.system/restaurant.service:$1 europe-central2-docker.pkg.dev/sunlit-vortex-459613-h0/food-ordering-system-repository/restaurant.service:$1
+
+docker push europe-central2-docker.pkg.dev/sunlit-vortex-459613-h0/food-ordering-system-repository/order.service:$1
+docker push europe-central2-docker.pkg.dev/sunlit-vortex-459613-h0/food-ordering-system-repository/customer.service:$1
+docker push europe-central2-docker.pkg.dev/sunlit-vortex-459613-h0/food-ordering-system-repository/payment.service:$1
+docker push europe-central2-docker.pkg.dev/sunlit-vortex-459613-h0/food-ordering-system-repository/restaurant.service:$1
